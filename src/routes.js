c2e070ec0eaddeefc9cf69377f2d7671e1d95c01
Matheus Acfer <matheus.acfer@nave.org.br>
2020-03-29 00:55:15 -0300

    backend dnv

---
+++ src/routes.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
+
+const ongCotroller = require('./controllers/ongController')
+const incidentController = require('./controllers/incidentController')
+const profileController = require('./controllers/profileController')
+const sessionController = require('./controllers/sessionController')
+
+const routes = express.Router()
+
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required()
+  })
+}), sessionController.create)
+
+routes.get('/ongs', ongCotroller.index)
+
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  })
+}), ongCotroller.create)
+
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown()
+}), profileController.index)
+
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}), incidentController.index)
+
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required()
+  })
+}), incidentController.create)
+
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  })
+}), incidentController.delete)
+
+module.exports = routes
